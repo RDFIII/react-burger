e785de4ea3c856ca92a7d315cad5bd963c2fc155
RDF <foulkesr@gmail.com>
2019-05-22 17:28:02 -0400

    create removeIngredientHandler and wire it to BuildControl/s

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,21 +3,26 @@ import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'Cheese'},
-    {label: 'Meat', type: 'meat '}
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        {controls.map(crtl => (
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
             <BuildControl 
-                key={crtl.label} 
-                label={crtl.label} 
-                added={() => props.ingredientAdded(crtl.type)}
-            />
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
         ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 );
 
